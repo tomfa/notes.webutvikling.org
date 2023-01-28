58cfeab4c846bf15aa51c531e7c61376678fe8ac
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2023-01-29 00:09:14 +0100

    config: set tailwind jit-mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,7 @@ const plugin = require("tailwindcss/plugin");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
 	content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
 	darkMode: "class",
 	corePlugins: {
