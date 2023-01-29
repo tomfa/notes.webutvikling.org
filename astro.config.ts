27f21d95b89f6828e023e82fb5adf21e103b7427
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2023-01-29 09:19:19 +0100

    build: add astro-react

--- astro.config.ts
+++ astro.config.ts
@@ -6,28 +6,25 @@ import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
 export default defineConfig({
-	site: "https://www.astro-theme-cactus.netlify.app",
-	experimental: {
-		contentCollections: true,
-	},
-	markdown: {
-		shikiConfig: {
-			theme: "dracula",
-			wrap: true,
-		},
-	},
-	integrations: [
-		mdx({}),
-		tailwind({
-			config: {
-				applyBaseStyles: false,
-			},
-		}),
-		image({
-			serviceEntryPoint: "@astrojs/image/sharp",
-		}),
-		sitemap(),
-		prefetch(),
-	],
+  site: "https://www.astro-theme-cactus.netlify.app",
+  experimental: {
+    contentCollections: true
+  },
+  markdown: {
+    shikiConfig: {
+      theme: "dracula",
+      wrap: true
+    }
+  },
+  integrations: [mdx({}), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), sitemap(), prefetch(), react()]
 });
