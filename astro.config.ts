4b0f4d3bd101460cc7bec09986623db8718d691a
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2023-01-29 09:51:48 +0100

    build: add astro-embed

--- astro.config.ts
+++ astro.config.ts
@@ -4,27 +4,38 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import embeds from "astro-embed/integration";
 
 // https://astro.build/config
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.astro-theme-cactus.netlify.app",
-  experimental: {
-    contentCollections: true
-  },
-  markdown: {
-    shikiConfig: {
-      theme: "dracula",
-      wrap: true
-    }
-  },
-  integrations: [mdx({}), tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  }), sitemap(), prefetch(), react()]
+	site: "https://www.astro-theme-cactus.netlify.app",
+	experimental: {
+		contentCollections: true,
+	},
+	markdown: {
+		shikiConfig: {
+			theme: "dracula",
+			wrap: true,
+		},
+	},
+	integrations: [
+		mdx({}),
+		tailwind({
+			config: {
+				applyBaseStyles: false,
+			},
+		}),
+		image({
+			serviceEntryPoint: "@astrojs/image/sharp",
+		}),
+		sitemap(),
+		prefetch(),
+		react(),
+		embeds(),
+	],
 });
