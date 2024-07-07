eaf0548f3fe7ff2da0479f2c7e47199751b4fa1b
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2024-07-07 10:20:22 +0200

    add og:image

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,6 +3,7 @@ const siteMeta = {
 	description: "Random notes, anecdotes and random scribbling from my tech life and work brain",
 	lang: "en-GB",
 	ogLocale: "en_GB",
+	image: "https://notes.webutvikling.org/images/notes.png",
 	githubUrl: "https://github.com/tomfa/notes",
 	themeColorLight: "#fafafa",
 	themeColorDark: "#1d1f21",
